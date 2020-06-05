78ca8b5bfe5eac6da28881af3306cc4fef9cc7e6
Akhil Nathoo <akhilnathoo123@gmail.com>
2020-06-05 12:38:08 -0400

    Added inRange

---
+++ _.js
@@ -0,0 +1,33 @@
+const _ ={
+
+    clamp(number, lower, upper)
+    {
+      const lowerClampedValue=Math.max(number, lower);
+      const clampedValue=Math.min(lowerClampedValue, upper);
+      return clampedValue;
+    },
+  
+    inRange(number, start, end)
+    {
+      if(end===undefined)
+      {
+        end=start
+        start=0
+      }
+      if (start>end)
+      {
+        var temp= end
+        end= start
+        start= temp
+      }
+      var isInRange= start<= number && number < end
+      return isInRange
+    }
+  
+  };
+
+
+
+
+// Do not write or modify code below this line.
+module.exports = _;
